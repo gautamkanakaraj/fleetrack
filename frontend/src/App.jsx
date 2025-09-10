5cdea7a6beac86b9636df89c3373125fe8b14798
gautamkanakaraj <gautam.kanakarajv@gmail.com>
2025-09-10 14:54:17 +0530

    required frontend files

---
+++ frontend/src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+
+
+function App() {
+
+ return (
+  <>
+  <H1> map of the esp-32 </H1>
+  </>
+  )
+   
+}
+
+export default App
